75eb4db79976f580f5c0f30b130492edeac03aa8
ARMOR-of-ODD <119148970+ARMOR-of-ODD@users.noreply.github.com>
2023-08-02 00:32:29 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,58 +1,20 @@
-package newone;
-
-import java.util.Scanner;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import java.util.Timer;
 
 public class Main {
-
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Welcome to the ATM");
+        try {
 
-        ATM atm = new ATM();
-
-        while (true) {
-            if (atm.login()) {
-                while (true) {
-
-                    System.out.println("Choose an option:");
-                    System.out.println("1. View transaction history");
-                    System.out.println("2. Withdraw");
-                    System.out.println("3. Deposit");
-                    System.out.println("4. Transfer");
-                    System.out.println("5. Quit");
-                    System.out.print("Choice: ");
-
-                    int choice = scanner.nextInt();
-
-                    switch (choice) {
-                        case 1:
-                            atm.showTransactionHistory();
-                            break;
-                        case 2:
-                            atm.doWithdraw();
-                            break;
-                        case 3:
-                            atm.doDeposit();
-                            break;
-                        case 4:
-                            atm.doTransfer();
-                            break;
-                        case 5:
-                            System.out.println("Goodbye");
-                            return;
-                        default:
-                            System.out.println("Invalid choice, please try again");
-                            break;
-                    }
-                }
-            }
-            else {
-                System.out.println("Invalid user ID or PIN, please try again");
-            }
-
+            Login form = new Login();
+            form.setSize(420, 240);
+            form.setVisible(true);
         }
-
+        catch(Exception e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
     }
-
 }
